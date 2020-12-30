34ff974ff13fdc75bb8ea4f333af5d14522c540d
Dhaval Halari <dhavalhalari213@gmail.com>
2020-12-31 00:11:36 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const {app,BrowserWindow} =  require("electron");
+app.on('ready', () => {
+ 
+  mainWindow = new BrowserWindow({
+      titleBarStyle: 'hidden',
+      width: 1920,
+      height: 1080,
+      show: false
+  });
+  
+  splash = new BrowserWindow({
+      width: 1920, height: 1080, transparent: false, alwaysOnTop: true
+    
+    });
+  splash.loadURL(`file://${__dirname}/splash.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+   mainWindow.once('ready-to-show', () => {
+    setTimeout(function(){ 
+        splash.destroy();
+        mainWindow.show();
+    }, 2000);
+})
+});
